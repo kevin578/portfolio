58400b5e7d15321ff114d00ecd663fbb4c168118
kevin578 <kbriggs@aapl000894.local>
2018-01-25 22:15:17 -0500

    working with navbar and header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./../css/Header.css";
+
+const Header = () => (
+    <div className = "header">
+    
+    </div>
+)
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import "./../css/Navbar.css"
+
+const Navbar = () => (
+    <div className="navbar">
+        <div className = "navbar__links">    
+            <span class="navbar__link">Home</span>
+            <span class="navbar__link">Projects</span>
+            <span class="navbar__link">About</span>
+            <span class="navbar__link">Contact</span> 
+        </div>    
+    </div>    
+)
+
+export default Navbar;
