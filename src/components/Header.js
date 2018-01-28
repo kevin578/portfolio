ccf3ccb88edc1dde69e527a35795df7d61146a60
kevin578 <kbriggs@aapl000894.local>
2018-01-28 10:06:58 -0500

    have picture in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./../css/Header.css";
+import profile from "./../img/profile.jpg";
 
 const Header = () => (
-    <div className = "header">
-    
+    <div className="header">  
+        <div className = "header__content">    
+            <img src={profile} class="header__content__img" />
+            <h1 className = "header__content__name">Kevin Briggs</h1>
+            <h2 className = "header__content__description">Front End Developer & Teacher</h2> 
+        </div>
     </div>
 )
 
