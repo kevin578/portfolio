60cf15ed5c2be25b7fb7f9d070ec856740c0cd4b
kevin578 <kbriggs@MA000894-KBRIGGS.home>
2018-02-20 20:07:27 -0500

    have icons arranged

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,44 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import Navbar from "./Navbar";
 import Header from "./Header";
 import Projects from "./Projects";
-import About from './About';
-import Contact from './Contact';
-import scrollToComponent from 'react-scroll-to-component';
-import './../css/App.css';
-
+import About from "./About";
+import Contact from "./Contact";
+import scrollToComponent from "react-scroll-to-component";
+import "./../css/App.css";
 
 class App extends Component {
-
-  scrollDown = (scrollHref, time= 800) => {
-    console.log("run")
+  scrollDown = (scrollHref, time = 800) => {
+    console.log("run");
     scrollToComponent(this[scrollHref], {
-      ease: 'outCirc',
+      ease: "outCirc",
       offset: -50,
-      align: 'top',
+      align: "top",
       duration: time
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <Navbar scrollDown={this.scrollDown} /> 
-        <Header scrollDown={this.scrollDown} ref={(section) => { this.Header = section; }}  />
-        <Projects ref={(section) => { this.Projects = section; }}/>
-        <About ref={(section) => { this.About = section; }} />
-        <Contact ref={(section) => { this.Contact = section; }}/>
+        <Navbar scrollDown={this.scrollDown} />
+        {/*<Header scrollDown={this.scrollDown} ref={(section) => { this.Header = section; }}  />*/}
+        <Projects
+          ref={section => {
+            this.Projects = section;
+          }}
+        />
+        <About
+          ref={section => {
+            this.About = section;
+          }}
+        />
+        <Contact
+          ref={section => {
+            this.Contact = section;
+          }}
+        />
       </div>
     );
   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,14 +6,17 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <div className="contact__icons">
-          <FontAwesome name="github" />
-          <FontAwesome name="youtube" />
-          <FontAwesome name="linkedin" />
-          <FontAwesome className="fas" name="envelope" />
+        <div className="contact__info">
+          <div className="contact__info__icons">
+            <FontAwesome className="fas" name="github" />
+            <FontAwesome className="fas" name="youtube" />
+            <FontAwesome className="fas" name="linkedin" />
+          </div>
+          <p className="contact__info__text">kevinbriggs1@gmail.com</p>
+          <p className="contact__info__text">Worcester, MA 01604</p>
         </div>
 
-        <div className="contact_form">
+        <div className="contact__form">
           <form
             method="POST"
             action="https://formspree.io/kevinbriggs1@gmail.com"
